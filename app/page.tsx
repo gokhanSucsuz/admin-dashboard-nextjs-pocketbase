0cb75d056e93b889e1d63695f343e5c7cafbddad
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-08-28 22:42:29 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -22,24 +22,30 @@ export default function Home() {
 		router.refresh();
 		router.push("/");
 	};
-	return (
-		<div>
-			{user
-				? <div className="flex flex-col justify-center items-center">
-						<div>
-							{user.username}
-						</div>
-						<div>
-							{user.name}
-						</div>
-						<div>
-							{user.email}
-						</div>
-						<Button variant="default" onClick={handleLogout}>
-							Logout
-						</Button>
-					</div>
-				: <div>User not logged!</div>}
-		</div>
-	);
+	return <div className="flex flex-col justify-center items-center h-screen">
+			<div className="p-10 shadow-lg border rounded-lg space-y-10">
+				{user ? <div className="flex flex-col justify-center items-center">
+							<div>
+								{user.username}
+							</div>
+							<div>
+								{user.name}
+							</div>
+							<div>
+								{user.email}
+							</div>
+							<Button variant="default" onClick={handleLogout}>
+								Logout
+							</Button>
+						</div> : <div>User not logged!</div>}
+				<div className="flex justify-between items-center space-x-5">
+					<Button variant="destructive" onClick={() => router.push("/register")}>
+						Register
+					</Button>
+					<Button variant="destructive" onClick={() => router.push("/login")}>
+						Login
+					</Button>
+				</div>
+			</div>
+		</div>;
 }
